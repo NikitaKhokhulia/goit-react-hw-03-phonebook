e569c28ef571858d9eae68c07e16bdb5eeeea6ab
Nikita Khokhulia <khokhulianikita@gmail.com>
2023-02-01 15:51:43 +0200

    add main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -48,6 +48,21 @@ export default class App extends Component {
     console.log(contactsId);
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contact) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     const { filter } = this.state;
     const {
